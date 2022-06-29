3090d5ca0484e219ea6a0215acc7e064a0453554
Cristian Tironi <ctironi@estudiante.ceibal.edu.uy>
2022-06-29 11:38:11 -0300

    Ejercicio 1 listo

--- src/main/java/com/sofka/proyect/Ejercicio1.java
+++ src/main/java/com/sofka/proyect/Ejercicio1.java
@@ -1,4 +1,20 @@
 package com.sofka.proyect;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingresar nro:");
+        int nro = sc.nextInt();
+
+
+        if((nro % 2) != 0){
+            System.out.println("El numero es Impar" );
+        }else if ((nro % 2) == 0){
+            System.out.println("El numero es Par");
+        }
+    }
 }
